4c415cda732dc019089086e5a162089a38b26e9b
Mac Macovič <maciii@gmail.com>
2018-09-04 20:40:59 +0200

    CZ verze a curl místo wget

--- Enigma2_Bouquet_Picon_Update.sh
+++ Enigma2_Bouquet_Picon_Update.sh
@@ -16,62 +16,63 @@ PURPLE='\c00?:55>7'
 WHITE='\c00??????'
 
 #Change the URL to point to your Bouquet tar.gz file
-BQ="https://github.com/maciii/E2BouquetUpdate/releases/download/v1.0/enigma2.tar"
+BQ="https://github.com/maciii/E2BouquetUpdate/releases/download/v1.0/enigma2.tar.gz"
 BP="https://github.com/maciii/E2BouquetUpdate/releases/download/v1.0/picon.tar.gz"
 BE="https://github.com/maciii/E2BouquetUpdate/releases/download/v1.0/encoding.tar.gz"
 
-echo $YELLOW
-echo "Please be patient, this process will take a little moment ;)."
-#sleep 3
-echo "OK, now the update starts."
-echo $PURPLE
-echo "Please wait..."
-echo $WHITE
-sleep 3
 
+echo $YELLOW
+echo "AKTUALIZACE PROGRAMOVÉ NABÍDKY"
+#sleep 3
+echo "Buďte trpěliví, bude to chvilku trvat ;)"
+#sleep 2
+echo $PURPLE
+echo "Zahajuji aktualizaci, prosím čekejte..."
+echo $WHITE
+sleep 2
 
 ## Bouquet Download & Installation ##
 cd /tmp
-wget $BQ
+curl $BQ -Lko /tmp/enigma2.tar.gz
 chmod 755 /tmp/enigma2.tar.gz
 if find /tmp/enigma2.tar.gz; then
-tar -xzvf enigma2.tar.gz
+  tar -xzvf enigma2.tar.gz
 
-cd /tmp/enigma2
-rm -rf /etc/tuxbox/satellites.xml
-cp /tmp/enigma2/satellites.xml /etc/tuxbox
-cd /etc/tuxbox
-chmod 755 satellites.xml
+  cd /tmp/enigma2
+  rm -rf /etc/tuxbox/satellites.xml
+  cp /tmp/enigma2/satellites.xml /etc/tuxbox
+  cd /etc/tuxbox
+  chmod 755 satellites.xml
 
-cd /etc/enigma2
-rm -rf *.tv
-rm -rf *.radio
-rm -rf blacklist
-rm -rf lamedb
-rm -rf cables.xml
-rm -rf satellites.xml
-rm -rf terrestrial.xml
-rm -rf SHOUTcast.favorites
-mv /tmp/enigma2/* /etc/enigma2
-cd /etc/enigma2
-chmod 755 satellites.xml
+  cd /etc/enigma2
+  rm -rf *.tv
+  rm -rf *.radio
+  rm -rf blacklist
+  rm -rf lamedb
+  rm -rf cables.xml
+  rm -rf satellites.xml
+  rm -rf terrestrial.xml
+  rm -rf SHOUTcast.favorites
+  mv /tmp/enigma2/* /etc/enigma2
+  cd /etc/enigma2
+  chmod 755 satellites.xml
 
-rm -rf /tmp/enigma2.tar.gz
-rm -rf /tmp/enigma2
+  rm -rf /tmp/enigma2.tar.gz
+  rm -rf /tmp/enigma2
 
-echo
-echo $GREEN
-echo "Enigma2 Setting is updated."
-echo $WHITE
-sleep 3
+  echo
+  echo $GREEN
+  echo "Seznam programů byl úspěšně aktualizován."
+  echo $WHITE
+  sleep 3
 
 else
 
-echo $RED
-echo "Error: Server is not available..."
-echo "Please wait..."
-echo $WHITE
-sleep 1
+  echo $RED
+  echo "CHYBA: Server je nedostupný..."
+  echo "Prosím čekejte..."
+  echo $WHITE
+  sleep 1
 
 fi
 ## Picon Download & Installation ##
@@ -80,79 +81,80 @@ rm -rf *.png
 rm -rf *.db
 sleep 1
 cd /tmp
-wget $BP
+curl $BP -Lko /tmp/picon.tar.gz
 chmod 755 /tmp/picon.tar.gz
 if find /tmp/picon.tar.gz; then
-tar -xzvf picon.tar.gz
-rm -rf /usr/share/enigma2/picon
-mv /tmp/picon /usr/share/enigma2
-chmod 755 /usr/share/enigma2/picon
+  tar -xzvf picon.tar.gz
+  rm -rf /usr/share/enigma2/picon
+  mv /tmp/picon /usr/share/enigma2
+  chmod 755 /usr/share/enigma2/picon
 
-rm -rf /tmp/picon.tar.gz
-rm -rf /tmp/picon
-cd /usr/share/enigma2/picon
-rm -rf *db
+  rm -rf /tmp/picon.tar.gz
+  rm -rf /tmp/picon
+  cd /usr/share/enigma2/picon
+  rm -rf *db
 
-echo
-echo $GREEN
-echo "Picons are updated."
-echo $WHITE
-sleep 3
+  echo
+  echo $GREEN
+  echo "Ikony programů byly aktualizovány."
+  echo $WHITE
+  sleep 3
 
 else
 
-echo $RED
-echo "Error: Server is not available..."
-echo "Please wait..."
-echo $WHITE
-sleep 1
+  echo $RED
+  echo "CHYBA: Server je nedostupný..."
+  echo "Prosím čekejte..."
+  echo $WHITE
+  sleep 1
 
 fi
-## Encoding Download & Installation ##
-# cd /tmp
-# wget $BE
-# chmod 755 /tmp/encoding.tar.gz
-# if find /tmp/encoding.tar.gz; then
-# tar -xzvf encoding.tar.gz
-#
-# rm -rf /usr/share/enigma2/encoding.conf
-# mv /tmp/encoding.conf /usr/share/enigma2
-#
-# rm -rf /tmp/encoding.tar.gz
-# rm -rf /tmp/encoding.conf
-#
-# echo
-# echo $GREEN
-# echo "Encoding is updated."
-# echo $WHITE
-# echo
-# echo
-# sleep 3
 
-## Reload Settings & Restart BOX ##
-sleep 2
-echo $GREEN
-echo "Done! :) Your BOX is now going to GUI restart."
-sleep 2
-echo $YELLOW
-echo "Please wait for GUI restart."
-sleep 4
-killall -9 enigma2
+## Encoding Download & Installation ##
+cd /tmp
+wget $BE
+chmod 755 /tmp/encoding.tar.gz
+if find /tmp/encoding.tar.gz; then
+  tar -xzvf encoding.tar.gz
+
+  rm -rf /usr/share/enigma2/encoding.conf
+  mv /tmp/encoding.conf /usr/share/enigma2
+
+  rm -rf /tmp/encoding.tar.gz
+  rm -rf /tmp/encoding.conf
+
+  echo
+  echo $GREEN
+  echo "Konfigurace je aktualizována."
+  echo $WHITE
+  echo
+  echo
+  sleep 3
+
+  ## Reload Settings & Restart BOX ##
+  sleep 2
+  echo $GREEN
+  echo "Hotovo! :) Váš přístroj se nyní restartuje."
+  sleep 2
+  echo $YELLOW
+  echo "Prosím čekejte než proběhne restart."
+  sleep 4
+  killall -9 enigma2
 
 else
 
-echo $RED
-echo "Error: Server is not available..."
-sleep 2
-echo
-echo $PURPLE
-echo "It's likely that the update server is unavailable..."
-sleep 3
-echo "...or Your BOX is not connected to the network."
-sleep 2
-echo $YELLOW
-echo "Please check your network connection or try repeating the process later."
-echo
-echo $GREEN "Now press >>"$YELLOW" OK "$GREEN"<< or >>"$YELLOW" Exit "$GREEN"<< key."
+  echo $RED
+  echo "CHYBA: Server je nedostupný..."
+  sleep 2
+  echo
+  echo $PURPLE
+  echo "Aktualizační server je pravděpodobně nedostupný..."
+  sleep 3
+  echo "...nebo je problém s připojením k internetu."
+  sleep 2
+  echo $YELLOW
+  echo "Prosím ověřte vaše internetové připojení nebo zkuste aktualizaci později."
+  echo
+  echo $GREEN "Nyní stiskněte tlačítko >>"$YELLOW" OK "$GREEN"<< nebo >>"$YELLOW" Exit "$GREEN"<<"
 fi
 exit
--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 "# E2BouquetUpdate"
 
-Postavný na pluginu Enigma2 Bouquet & Picon Update Plugin
+Postavný na skriptu Enigma2 Bouquet & Picon Update Plugin
 
-ok first off i am not the author of this plugin, that honour goes to rossi2000 of DBUK. also i am posting this here in the VU plugins section but it will work with any E2 system such as a dreambox etc.
+Skript pro aktualizaci bouquetů a picon v systému Enigma2
 
-Plugin pro aktualizaci bouquetů a picon v systému Enigma2
+Skript nakopírujeme do adresáře /usr/script v našem boxu a například pomocí telnetu mu dáme práva na spouštění:
 
-Script je v \usr\lib\enigma2\python\Plugins\SystemPlugins\E2_Bouquet_Picon_Update
+chmod 755 /usr/script/nas_script.sh
+
+Pokud adresář /usr/script neexistuje, vytvoříme ho pomocí telnetu:
+mkdir /usr/script
+
+následně přiřadíme tlačítku dálkového ovládání např. na dlouhý stisk červeného tlačítka:
+
+MENU/Nastavení/Přizpůsobení a vzhled/Nastavení tlačítek DO   - nyní dlouze podržíme červené tlačítko
+
+Vybereme ShellScripts/nas_script.sh a uložíme Zeleným tlačítkem
+
+Skript se spustí dlouhým stiskem červeného tlačítka
